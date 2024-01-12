05a73203e9b61ed75cc996d8c9e7c506558fb044
凌览 <lihai_js@163.com>
2024-01-12 21:55:07 +0800

    chore: 注释创建标签功能

--- src/db/db.js
+++ src/db/db.js
@@ -4,5 +4,5 @@ export const db = new Dexie('gist')
 
 db.version(2).stores({
     gistTable: '&gistId,code,language,description,editTime,tags', // Primary key and indexed props
-    tags: '++tagId,tag'
+    // tags: '++tagId,tag'
 });
