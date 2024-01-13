14af65e1ec2ab02145168314620b3415bc39014f
凌览 <lihai_js@163.com>
2024-01-13 15:01:09 +0800

    fix: 首页宽度超出

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -29,48 +29,53 @@
         </div>
       </a-affix>
 
-
-      <template v-if="!gistParams.loading">
-        <div v-for="l in gistList" :key="l.gistId" class="pb-4 select-none" @click="onClickGistListItem(l.gistId)">
-          <div class="flex py-2">
-            <icon-code size="32px" style="flex-shrink: 0;" />
-            <div class="pl-2 grow text-nowrap truncate">
-              <a-link class="text-base font-semibold" :hoverable="false">{{ 'Gist:' + l.gistId }}</a-link>
-              <p class="m-0 text-xs truncate" style="color:rgb(101, 109, 118);">{{ '编辑于 ' +
-                dayjs(l.editTime).fromNow() }}</p>
-              <p class="m-0 text-xs truncate" style="color:rgb(101, 109, 118);width: 100%;">{{ l.description }}</p>
+      <div style="width: calc(100vw - 290px);">
+        <template v-if="!gistParams.loading">
+          <div v-for="l in gistList" :key="l.gistId" class="pb-4 select-none" @click="onClickGistListItem(l.gistId)">
+            <div class="flex py-2">
+              <icon-code size="32px" style="flex-shrink: 0;" />
+              <div class="pl-2 grow text-nowrap truncate">
+                <a-link class="text-base font-semibold" :hoverable="false">{{ 'Gist:' + l.gistId }}</a-link>
+                <p class="m-0 text-xs truncate" style="color:rgb(101, 109, 118);">{{ '编辑于 ' +
+                  dayjs(l.editTime).fromNow() }}</p>
+                <p class="m-0 text-xs truncate" style="color:rgb(101, 109, 118);width: 100%;">{{ l.description }}</p>
+              </div>
+            </div>
+            <div class="code-hover border-solid border-transparent border rounded-md overflow-hidden cursor-pointer">
+              <code-block :custom-style="{ maxHeight: '200px', overflowY: 'hidden' }" :code="l.code" :tags="l.tags"
+                :language="l.language" />
             </div>
           </div>
-          <div class="code-hover border-solid border-transparent border rounded-md overflow-hidden cursor-pointer">
-            <code-block :custom-style="{ maxHeight: '200px', overflowY: 'hidden' }" :code="l.code" :tags="l.tags"
-              :language="l.language" />
-          </div>
+        </template>
+
+        <a-skeleton v-else :animation="true">
+          <a-space direction="vertical" :style="{ width: '100%' }" size="large">
+            <a-skeleton-line :rows="5" />
+          </a-space>
+        </a-skeleton>
+
+        <div v-show="!gistParams.total && !gistParams.loading" class="mt-8">
+          <a-empty description=" " class="p-0" />
+          <p class="text-center text-sm m-0" style="color:rgb(120,120,122);"><span>无数据,</span>
+            <a-button type="text" class="p-0 inline" @click="onClickCreate">立即创建</a-button>
+          </p>
         </div>
-      </template>
-
-      <a-skeleton v-else :animation="true">
-        <a-space direction="vertical" :style="{ width: '100%' }" size="large">
-          <a-skeleton-line :rows="5" />
-        </a-space>
-      </a-skeleton>
-
-      <div v-show="!gistParams.total && !gistParams.loading" class="mt-8">
-        <a-empty description=" " class="p-0" />
-        <p class="text-center text-sm m-0" style="color:rgb(120,120,122);"><span>无数据,</span>
-          <a-button type="text" class="p-0 inline" @click="onClickCreate">立即创建</a-button>
-        </p>
+        <!-- v-show="gistParams.total > gistParams.size" -->
+        <div class="flex justify-center pb-4" style="margin-top: 4px;" v-show="gistParams.total > gistParams.size">
+          <a-button-group>
+            <a-button @click="onPrevPage" :disabled="gistParams.page === 1">
+              <icon-left />
+              上一页
+            </a-button>
+            <a-button @click="onNextPage" :disabled="(gistParams.size * gistParams.page) > gistParams.total">
+              下一页 <icon-right /></a-button>
+          </a-button-group>
+        </div>
       </div>
-      <!-- v-show="gistParams.total > gistParams.size" -->
-      <div class="flex justify-center pb-4" style="margin-top: 4px;" v-show="gistParams.total > gistParams.size">
-        <a-button-group>
-          <a-button @click="onPrevPage" :disabled="gistParams.page === 1">
-            <icon-left />
-            上一页
-          </a-button>
-          <a-button @click="onNextPage" :disabled="(gistParams.size * gistParams.page) > gistParams.total">
-            下一页 <icon-right /></a-button>
-        </a-button-group>
-      </div>
+
+
+
+
     </div>
   </div>
 </template>
