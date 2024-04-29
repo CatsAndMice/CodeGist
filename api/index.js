e2ec06f861da7701456c985213faed48fdc1ce20
凌览 <lihai_js@163.com>
2024-04-29 17:25:11 +0800

    build: 添加api接口

---
+++ api/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const { Router } = require('express');
+const app = express();
+const router = Router();
+
+router.get('/healthcheck', (_req, res) => {
+    res.status(200).json({ success: true, message: 'API is running' });
+})
+
+app.use("/api", router);
+
+app.listen(9090, () => console.log("Server ready on port 9090"));
+
+module.exports = app;
