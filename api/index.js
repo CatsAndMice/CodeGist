35f73fb68633e644e13731e0d277f33f83cca02a
凌览 <lihai_js@163.com>
2024-04-30 17:35:08 +0800

    test: 测试

--- api/index.js
+++ api/index.js
@@ -5,21 +5,24 @@ const app = express();
 const router = Router();
 app.use(express.json())
 router.get('/healthcheck', (_req, res) => {
-    const body = _req.json(_req.body)
-    console.log(_req.body,body.sessionWebhook)
-    axios.post(body.sessionWebhook,{
-        contentType:'ai_card',
-        content:{
-            templateId:'38d66950-4638-446b-81c4-edddb08d12c3.schema',
-            cardData:{
-                content:'Hello World'
+    // const body = _req.json(_req.body)
+    console.log(_req.body)
+    if (_req.body.sessionWebhook) {
+        axios.post(_req.body.sessionWebhook, {
+            contentType: 'ai_card',
+            content: {
+                templateId: '38d66950-4638-446b-81c4-edddb08d12c3.schema',
+                cardData: {
+                    content: 'Hello World'
+                }
             }
-        }
-    },{
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
+        }, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+    }
+
 
 
     res.status(200).json({
@@ -30,6 +33,6 @@ router.get('/healthcheck', (_req, res) => {
 
 app.use("/api", router);
 
-app.listen(9090, () => console.log("Server ready on port 9090"));
+app.listen(9000, () => console.log("Server ready on port 9090"));
 
 module.exports = app;
