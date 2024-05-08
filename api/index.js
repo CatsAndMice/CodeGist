70a19e785a7c758aaf0b4280557ac071652ea190
凌览 <lihai_js@163.com>
2024-05-08 10:43:16 +0800

    test: 验证

--- api/index.js
+++ api/index.js
@@ -7,9 +7,9 @@ app.use(express.json())
 router.get('/healthcheck', (_req, res) => {
     // const body = _req.json(_req.body)
     console.warn(JSON.stringify(_req.body), JSON.stringify(_req.query), 'body')
-    if (_req.body.sessionWebhook) {
+    if (_req.query.sessionWebhook) {
 
-        axios.post(_req.body.sessionWebhook, {
+        axios.post(_req.query.sessionWebhook, {
             contentType: 'ai_card',
             content: {
                 templateId: '38d66950-4638-446b-81c4-edddb08d12c3.schema',
