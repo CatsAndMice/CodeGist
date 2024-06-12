4dc6df43dddddbfbe7c2ab47843825850593ea9e
凌览 <lihai_js@163.com>
2024-06-12 15:42:31 +0800

    feat: 增加接口

--- api/db.js
+++ api/db.js
@@ -1,3 +1,4 @@
 const { Pool } = require('pg');
+console.log(process.env.POSTGRES_URL );
 const pool = new Pool({ connectionString: process.env.POSTGRES_URL });
 module.exports = pool
--- api/wechat/index.js
+++ api/wechat/index.js
@@ -4,7 +4,9 @@ const axios = require('axios');
 const qs = require('qs');
 const path = require('path');
 const createUser = require('./createUser');
-require('dotenv').config({ path: path.join(__dirname, '../../.env.local') });
+const pool = require('../db.js');
+const { to } = require("await-to-js");
+const { isEmpty } = require('lodash');
 
 //登录
 router.get('/wechat/login', (res, req) => {
@@ -16,12 +18,21 @@ router.get('/wechat/login', (res, req) => {
             grant_type: 'authorization_code',
             secret: 'd4b8cdf97e63c98b22c12f62dfb11334'
         })
-        axios.get('https://api.weixin.qq.com/sns/jscode2session?' + params).then(result => {
-            createUser(result.data.openid)
+        axios.get('https://api.weixin.qq.com/sns/jscode2session?' + params).then(async result => {
+            const isSuccess = await createUser(result.data.openid)
+            console.log(isSuccess);
+            if (isSuccess) {
+                req.status(200).send({
+                    code: 200,
+                    data: result.data,
+                    msg: '请求成功'
+                })
+                return
+            }
             req.status(200).send({
                 code: 200,
-                data: result.data,
-                msg: '请求成功'
+                data: null,
+                msg: '创建用户失败'
             })
         }, err => {
             req.status(200).send({
@@ -43,9 +54,9 @@ router.get('/wechat/login', (res, req) => {
 //获取用户信息
 router.get('/wechat/info', async (res, req) => {
     const { openId } = res.query
-    const [err, result] = await to(pool.query(`select avatar,name,gender from t_user where open_id='${openId}';`));
+    const [err, result] = await to(pool.query(`select avatar,user_name,gender from t_user where open_id='${openId}';`));
     if (result) {
-        const data = result.raws[0]
+        const data = result.rows[0]
         req.status(200).send({
             code: 200,
             data,
@@ -88,9 +99,9 @@ router.post('/wechat/work', async (res, req) => {
         commute_length,
         working_hours,
         average_daily_firewood)
-        VALUES (${openId},
+        VALUES ('${openId}',
             ${result},
-            to_timestamp(${startWorkTime}),
+            ${startWorkTime},
             ${occupation},
             ${ditto},
             ${oppositeSex},
@@ -103,7 +114,7 @@ router.post('/wechat/work', async (res, req) => {
         )
         ON CONFLICT (open_id) DO UPDATE SET 
         result = ${result}, 
-        start_work_time = to_timestamp(${startWorkTime}),
+        start_work_time = ${startWorkTime},
         occupation=${occupation},
         ditto=${ditto},
         opposite_sex=${oppositeSex},
@@ -114,7 +125,6 @@ router.post('/wechat/work', async (res, req) => {
         working_hours =  ${workingHours},
         average_daily_firewood = ${averageDailyFirewood}
         ;`));
-
     if (err) {
         req.status(200).send({
             code: 500,
@@ -126,6 +136,52 @@ router.post('/wechat/work', async (res, req) => {
     req.status(200).send({
         code: 200,
         data: null,
+        msg: '请求成功'
+    })
+})
+
+router.get('/wechat/work', async (res, req) => {
+    const [err, result] = await to(pool.query(`
+    SELECT 
+        t_work.result,
+        t_work.start_work_time,
+        t_work.occupation,
+        t_work.ditto,
+        t_work.opposite_sex,
+        t_work.work_env,
+        t_work.qualification,
+        t_work.hours_fish,
+        t_work.commute_length,
+        t_work.working_hours,
+        t_work.average_daily_firewood,
+        t_user.user_name,
+        t_user.avatar,
+        t_user.gender
+    FROM 
+        t_work
+    RIGHT 
+        JOIN 
+            t_user 
+        ON 
+            t_work.open_id = t_user.open_id 
+        WHERE 
+                result IS NOT NULL 
+        ORDER BY 
+                t_work.result 
+        DESC 
+        LIMIT 100;`));
+
+    if (result && !isEmpty(result.rows)) {
+        req.status(200).send({
+            code: 200,
+            data: result.rows,
+            msg: '请求成功'
+        })
+        return
+    }
+    req.status(200).send({
+        code: 200,
+        data: [],
         msg: '请求成功'
     })
 })
