7362efa9cdd9d74e675ef725be3c474f1db4dbae
凌览 <lihai_js@163.com>
2024-06-12 16:09:02 +0800

    build: 数据库连接添加ssl

--- api/db.js
+++ api/db.js
@@ -1,4 +1,4 @@
 const { Pool } = require('pg');
-console.log(process.env.POSTGRES_URL );
-const pool = new Pool({ connectionString: process.env.POSTGRES_URL });
+console.log(process.env.POSTGRES_URL);
+const pool = new Pool({ connectionString: process.env.POSTGRES_URL + '?sslmode=require' });
 module.exports = pool
