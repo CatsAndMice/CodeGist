48d99ee3f488badb2eb325d38d3e390b7293fe14
凌览 <lihai_js@163.com>
2024-06-20 09:33:17 +0800

    chore: 优化代码

--- api/wechat/index.js
+++ api/wechat/index.js
@@ -7,6 +7,7 @@ const createUser = require('./createUser');
 const pool = require('../db.js');
 const { to } = require("await-to-js");
 const { isEmpty } = require('lodash');
+const { selectWorkSql, getInfoSql } = require('./sql');
 
 //登录
 router.get('/wechat/login', (res, req) => {
@@ -54,7 +55,7 @@ router.get('/wechat/login', (res, req) => {
 //获取用户信息
 router.get('/wechat/info', async (res, req) => {
     const { openId } = res.query
-    const [err, result] = await to(pool.query(`select avatar,user_name,gender from t_user where open_id='${openId}';`));
+    const [err, result] = await to(pool.query(getInfoSql(openId)));
     if (result) {
         const data = result.rows[0]
         req.status(200).send({
@@ -72,59 +73,7 @@ router.get('/wechat/info', async (res, req) => {
 })
 
 router.post('/wechat/work', async (res, req) => {
-    const {
-        averageDailyFirewood,
-        workingHours,
-        commuteLength,
-        hoursFish,
-        qualification,
-        workEnv,
-        oppositeSex,
-        ditto,
-        occupation,
-        startWorkTime,
-        result,
-        openId
-    } = res.body
-    const [err] = await to(pool.query(`INSERT INTO t_work (
-        open_id,
-        result,
-        start_work_time,
-        occupation,
-        ditto,
-        opposite_sex,
-        work_env,
-        qualification,
-        hours_fish,
-        commute_length,
-        working_hours,
-        average_daily_firewood)
-        VALUES ('${openId}',
-            ${result},
-            ${startWorkTime},
-            ${occupation},
-            ${ditto},
-            ${oppositeSex},
-            ${workEnv},
-            ${qualification},
-            ${hoursFish},
-            ${commuteLength},
-            ${workingHours},
-            ${averageDailyFirewood}
-        )
-        ON CONFLICT (open_id) DO UPDATE SET 
-        result = ${result}, 
-        start_work_time = ${startWorkTime},
-        occupation=${occupation},
-        ditto=${ditto},
-        opposite_sex=${oppositeSex},
-        work_env=${workEnv},
-        qualification=${qualification},
-        hours_fish=${hoursFish},
-        commute_length=${commuteLength},
-        working_hours =  ${workingHours},
-        average_daily_firewood = ${averageDailyFirewood}
-        ;`));
+    const [err] = await to(pool.query(getInsertWorkSql(res.body)));
 
     if (err) {
         req.status(200).send({
@@ -143,37 +92,7 @@ router.post('/wechat/work', async (res, req) => {
 })
 
 router.get('/wechat/work', async (res, req) => {
-    const [err, result] = await to(pool.query(`
-    SELECT 
-       t_work.open_id,
-        t_work.result,
-        t_work.start_work_time,
-        t_work.occupation,
-        t_work.ditto,
-        t_work.opposite_sex,
-        t_work.work_env,
-        t_work.qualification,
-        t_work.hours_fish,
-        t_work.commute_length,
-        t_work.working_hours,
-        t_work.average_daily_firewood,
-        t_user.user_name,
-        t_user.avatar,
-        t_user.gender
-    FROM 
-        t_work
-    RIGHT 
-        JOIN 
-            t_user 
-        ON 
-            t_work.open_id = t_user.open_id 
-        WHERE 
-                result IS NOT NULL 
-        ORDER BY 
-                t_work.result 
-        DESC 
-        LIMIT 100;`));
-    console.log(err)
+    const [err, result] = await to(pool.query(selectWorkSql));
     if (result && !isEmpty(result.rows)) {
         req.status(200).send({
             code: 200,
---
+++ api/wechat/sql.js
@@ -0,0 +1,92 @@
+
+// 获取用户信息
+exports.getInfoSql = (openId) => {
+    return `select avatar,user_name,gender,open_id from t_user where open_id='${openId}';`
+}
+// 插入数据到排行榜表
+exports.getInsertWorkSql = (body) => {
+    const {
+        averageDailyFirewood,
+        workingHours,
+        commuteLength,
+        hoursFish,
+        qualification,
+        workEnv,
+        oppositeSex,
+        ditto,
+        occupation,
+        startWorkTime,
+        result,
+        openId
+    } = body
+    return `INSERT INTO t_work (
+        open_id,
+        result,
+        start_work_time,
+        occupation,
+        ditto,
+        opposite_sex,
+        work_env,
+        qualification,
+        hours_fish,
+        commute_length,
+        working_hours,
+        average_daily_firewood)
+        VALUES ('${openId}',
+            ${result},
+            ${startWorkTime},
+            ${occupation},
+            ${ditto},
+            ${oppositeSex},
+            ${workEnv},
+            ${qualification},
+            ${hoursFish},
+            ${commuteLength},
+            ${workingHours},
+            ${averageDailyFirewood}
+        )
+        ON CONFLICT (open_id) DO UPDATE SET 
+        result = ${result}, 
+        start_work_time = ${startWorkTime},
+        occupation=${occupation},
+        ditto=${ditto},
+        opposite_sex=${oppositeSex},
+        work_env=${workEnv},
+        qualification=${qualification},
+        hours_fish=${hoursFish},
+        commute_length=${commuteLength},
+        working_hours =  ${workingHours},
+        average_daily_firewood = ${averageDailyFirewood}
+        ;`
+}
+
+// 排行榜
+exports.selectWorkSql = `SELECT 
+   t_work.open_id,
+    t_work.result,
+    t_work.start_work_time,
+    t_work.occupation,
+    t_work.ditto,
+    t_work.opposite_sex,
+    t_work.work_env,
+    t_work.qualification,
+    t_work.hours_fish,
+    t_work.commute_length,
+    t_work.working_hours,
+    t_work.average_daily_firewood,
+    t_user.user_name,
+    t_user.avatar,
+    t_user.gender
+FROM 
+    t_work
+RIGHT 
+    JOIN 
+        t_user 
+    ON 
+        t_work.open_id = t_user.open_id 
+    WHERE 
+            result IS NOT NULL 
+    ORDER BY 
+            t_work.result 
+    DESC 
+    LIMIT 100;`
