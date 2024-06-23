13784633c6e3eb077931eb1c1e06d9c5e69443dc
凌览 <lihai_js@163.com>
2024-06-23 22:25:25 +0800

    feat: 优化接口

--- api/wechat/sql.js
+++ api/wechat/sql.js
@@ -3,6 +3,36 @@
 exports.getInfoSql = (openId) => {
     return `select avatar,user_name,gender,open_id from t_user where open_id='${openId}';`
 }
+
+exports.getSelectUserWorkSql = (openId) => {
+    return `SELECT 
+    t_work.open_id,
+    t_work.result,
+    t_work.start_work_time,
+    t_work.occupation,
+    t_work.ditto,
+    t_work.opposite_sex,
+    t_work.work_env,
+    t_work.qualification,
+    t_work.hours_fish,
+    t_work.commute_length,
+    t_work.working_hours,
+    t_work.average_daily_firewood,
+    t_user.user_name,
+    t_user.avatar,
+    t_user.gender,
+    ROW_NUMBER() OVER (ORDER BY t_work.result DESC) AS ranking
+FROM 
+    t_user
+RIGHT 
+    JOIN 
+        t_work   
+    ON 
+        t_user.open_id = t_work.open_id
+    WHERE 
+        t_user.open_id ='${openId}';`
+}
+
 // 插入数据到排行榜表
 exports.getInsertWorkSql = (body) => {
     const {
@@ -60,6 +90,7 @@ exports.getInsertWorkSql = (body) => {
         ;`
 }
 
+
 // 排行榜
 exports.selectWorkSql = `SELECT 
    t_work.open_id,
@@ -76,7 +107,8 @@ exports.selectWorkSql = `SELECT
     t_work.average_daily_firewood,
     t_user.user_name,
     t_user.avatar,
-    t_user.gender
+    t_user.gender,
+    ROW_NUMBER() OVER (ORDER BY t_work.result DESC) AS ranking
 FROM 
     t_work
 RIGHT 
